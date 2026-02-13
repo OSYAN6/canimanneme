e04323c40eb1908a848b939d5a798c4d728a5948
Osman Can Kılınç <hiranurkilinc66@gmail.com>
2026-02-13 21:07:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById('openBtn').addEventListener('click', function() {
     document.getElementById('envelope').classList.add('open');
 });
+
+// Mobil cihazlar için dokunma desteği
+document.getElementById('openBtn').addEventListener('touchstart', function(e) {
+    e.preventDefault(); // Tıklama ile çakışmayı önler
+    document.getElementById('envelope').classList.add('open');
+});
